832d6a5bc5fcac58e1c47887ff610df225273782
Lam Nguyen <lamlnguyen@yahoo.com>
2018-01-06 03:07:55 -0800

    adds gaussian blurring to smooth peaks, minor cleanup

--- heightmap.py
+++ heightmap.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python
 import math
 import png
 import random
-
+import numpy as np
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+from scipy import ndimage as nd
 n = 7
-rough = 0.5
+rough = 0.4
 roughness = rough * 127 / (math.pow(2, n - 2))
+smooth = 0.7
 size = int(math.pow(2, n) + 1)
 s = [[127 for j in range(size)] for i in range(size)]
 
 def getVal(x, y):
+    # Get height values, wrapping around edges
     x = int(x) % size
     y = int(y) % size
     return s[x][y]
 
 def setVal(x, y, val):
+    # Set height values, wrapping around edges and clipping at 0 and 255
     x = int(x) % size
     y = int(y) % size
     if val > 255:
@@ -23,14 +30,17 @@ def setVal(x, y, val):
     s[x][y] = val
 
 def square(x, y, stepsize):
+    # Average the values of 4 corners of a square, add some randomness, and set the center value
     avg = getVal(x - stepsize, y - stepsize) + getVal(x + stepsize, y + stepsize) + getVal(x - stepsize, stepsize + y) + getVal(x + stepsize, stepsize - y)
-    setVal(x, y, int(avg/4 + roughness * (random.random() * 2 * stepsize - stepsize)))
+    setVal(x, y, int(avg/4.0 + roughness * (np.random.normal() * 2 * stepsize)))
 
 def diamond(x, y, stepsize):
+    # Average the values of 4 corners of a diamond, add some randomness, and set the center value
     avg = getVal(x - stepsize, y) + getVal(x, y + stepsize) + getVal(x + stepsize, y) + getVal(x, y - stepsize)
-    setVal(x, y, int(avg/4 + roughness * (random.random() * 2 * stepsize - stepsize)))
+    setVal(x, y, int(avg/4.0 + roughness * (np.random.normal() * 2 * stepsize)))
 
 def divide(i):
+    # Square and diamond recursively until done
     half = int(i / 2)
     if half == 0:
         return
@@ -44,66 +54,38 @@ def divide(i):
 
 divide(size - 1)
 
-def water(val):
-    if val > 127:
-        return val
-    else:
-        return 0
-# s = [[water(s[i][j]) for j in range(size)] for i in range(size)]
+s = nd.gaussian_filter(s, sigma=smooth)
 
 f = open('png.png', 'wb')
 w = png.Writer(len(s[0]), len(s), greyscale=True)
 w.write(f, s)
 f.close()
-for x in range(size):
-    for y in range(size):
-        print('[' + str(x) + ',' + str(y) + ',' + str(s[x][y] - 127) + ']')
-
-
-# For each set of style and range settings, plot n random points in the box
-# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
-
-#!/usr/bin/env python
-import pyqtgraph as pg
-import numpy as np
-
-coord_list = []
-for x in range(size):
-    for y in range(size):
-        coord_list.append((x * 10, y * 10, s[x][y]))
+# for x in range(size):
+#     for y in range(size):
+#         print('[' + str(x) + ',' + str(y) + ',' + str(s[x][y] - 127) + ']')
 
 pg.mkQApp()
-import pyqtgraph.opengl as gl
 view = gl.GLViewWidget()
+view.setCameraPosition(distance=1500)
 view.show()
 
 zgrid = gl.GLGridItem()
 surfacemap = gl.GLSurfacePlotItem(shader='shaded')
 surfacemap.setData(z=np.array(s))
+surfacemap.translate(-size*5, -size*5, 0)
 surfacemap.scale(10, 10, 1)
 view.addItem(zgrid)
 
-water2 = gl.GLSurfacePlotItem(shader='shaded')
+water = gl.GLSurfacePlotItem(shader='shaded')
 waterdata = np.zeros((size, size))
 for x in range(size):
     for y in range(size):
         waterdata[x, y] = 127
-water2.setData(z=waterdata)
-water2.scale(10, 10, 1)
-water2.setColor(np.array([0.2, 0.2, 0.8, 0.1]))
-view.addItem(water2)
-
-
-# water = gl.GLGridItem(color=np.array([0, 0, 1, 0]))
-# water.translate(size * 10 /2, size* 10 /2, 127)
-# water.scale(size/2, size/2, 1)
-# water.setSpacing(x=0.01, y=0.01)
-# view.addItem(water)
+water.setData(z=waterdata)
+water.scale(10, 10, 1)
+water.translate(-size*5, -size*5, 0)
+water.setColor(np.array([0.2, 0.2, 0.8, 0.1]))
+view.addItem(water)
 
 zgrid.scale(0.2, 0.2, 0.2)
-
-coords = np.array(coord_list)
-colors = (1, 1, 1, 1)
-# plt = gl.GLScatterPlotItem(pos=coords, color = colors, size=5, pxMode=True)
-# view.addItem(plt)
 view.addItem(surfacemap)
